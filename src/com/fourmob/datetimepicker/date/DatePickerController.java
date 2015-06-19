2df8f1d0b8bb2d976383f3901ee7902525eb95df
Shivansh Mittal <shivansh.mittal@hotcocoasoftware.com>
2015-06-19 18:29:09 +0530

    added setMinMaxDate method to date picker dialog

---
+++ src/com/fourmob/datetimepicker/date/DatePickerController.java
@@ -0,0 +1,27 @@
+package com.fourmob.datetimepicker.date;
+
+import java.util.Calendar;
+
+public abstract interface DatePickerController {
+    public abstract int getFirstDayOfWeek();
+
+    public abstract int getMaxYear();
+
+    public abstract int getMinYear();
+
+    public abstract SimpleMonthAdapter.CalendarDay getSelectedDay();
+
+    public abstract void onDayOfMonthSelected(int year, int month, int day);
+
+    public abstract void onYearSelected(int year);
+
+    public abstract void registerOnDateChangedListener(DatePickerDialog.OnDateChangedListener onDateChangedListener);
+
+    public abstract void tryVibrate();
+
+    int getTotalNumberOFMonthsToBeDisplayed();
+    int getMinDateMonth();
+
+    Calendar getMinDate();
+    Calendar getMaxDate();
+}
